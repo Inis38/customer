951ad8c7b63856115b019a1a1c7dce92dfe3ef26
inis38 <avm.irk@gmail.com>
2020-11-16 14:27:17 +0300

    Added log4j

--- src/main/java/edu/makarov/customer/CustomerApplication.java
+++ src/main/java/edu/makarov/customer/CustomerApplication.java
@@ -12,12 +12,6 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 public class CustomerApplication extends SpringBootServletInitializer {
 
-    private static final Logger logger = LogManager.getLogger(CustomerApplication.class);
-
-    public static void main(String[] args) {
-        SpringApplication.run(CustomerApplication.class, args);
-    }
-
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(CustomerApplication.class);
